3221961b5aef469837cfced09a910403d8586c6d
thainaspires <thainaspiress2@gmail.com>
2017-05-26 00:17:49 -0300

    App Exibir Texto
    App que exibe o texto digitado em outra activity

---
+++ Exibirtexto/app/src/main/java/com/thaina/exibirtexto/Main2Activity.java
@@ -0,0 +1,25 @@
+package com.thaina.exibirtexto;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class Main2Activity extends AppCompatActivity {
+    TextView texto2;
+    String stringDigit2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        texto2 = (TextView) findViewById(R.id.digitado);
+        stringDigit2 = getIntent().getExtras().getString("Value");
+        texto2.setText(stringDigit2);
+    }
+    public void btnClickAct2(View v){
+        Intent i = new Intent(Main2Activity.this, MainActivity.class);
+        startActivity(i);
+        finish();
+    }
+}
