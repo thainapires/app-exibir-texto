cc9b0d95e34c32e771fe43fff2d82836e85db2fa
thainaspires <thainaspiress2@gmail.com>
2017-06-09 13:35:00 -0300

    Clicar botão
    Quando clica no botão aparecem as frases/Imagens.

--- ExibirFrases/app/src/main/java/com/thaina/exibirfrases/MainActivity.java
+++ ExibirFrases/app/src/main/java/com/thaina/exibirfrases/MainActivity.java
@@ -2,7 +2,9 @@ package com.thaina.exibirfrases;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -17,23 +19,27 @@ public class MainActivity extends AppCompatActivity {
     String frase1, frase2, frase3;
     Button btn;
     TextView fra1, fra2, fra3;
+    ImageView img;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         fra1 = (TextView) findViewById(R.id.textView2);
         fra2 = (TextView) findViewById(R.id.textView3);
         fra3 = (TextView) findViewById(R.id.textView4);
         btn = (Button) findViewById(R.id.button2);
-
+    }
+    public void btnClicked(View v){
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myFirebaseRef = database.getInstance().getReference();
         myFirebaseRef.child("Salute/EN").addValueEventListener(new ValueEventListener() {
             public void onDataChange(DataSnapshot dataSnapshot) {
                 frase1 = dataSnapshot.getValue(String.class);
                 fra1.setText(frase1);
-
+                img = (ImageView)findViewById(R.id.usaFlag1);
+                img.setVisibility(View.VISIBLE);
+                img = (ImageView)findViewById(R.id.usaFlag2);
+                img.setVisibility(View.VISIBLE);
             }
 
             public void onCancelled(DatabaseError error) {
@@ -44,6 +50,10 @@ public class MainActivity extends AppCompatActivity {
             public void onDataChange(DataSnapshot dataSnapshot) {
                 frase2 = dataSnapshot.getValue(String.class);
                 fra2.setText(frase2);
+                img = (ImageView)findViewById(R.id.espFlag1);
+                img.setVisibility(View.VISIBLE);
+                img = (ImageView)findViewById(R.id.espFlag2);
+                img.setVisibility(View.VISIBLE);
 
             }
 
@@ -55,6 +65,10 @@ public class MainActivity extends AppCompatActivity {
             public void onDataChange(DataSnapshot dataSnapshot) {
                 frase3 = dataSnapshot.getValue(String.class);
                 fra3.setText(frase3);
+                img = (ImageView)findViewById(R.id.brFlag1);
+                img.setVisibility(View.VISIBLE);
+                img = (ImageView)findViewById(R.id.brFlag2);
+                img.setVisibility(View.VISIBLE);
 
             }
 
@@ -62,8 +76,6 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-
-
     }
 
 }
