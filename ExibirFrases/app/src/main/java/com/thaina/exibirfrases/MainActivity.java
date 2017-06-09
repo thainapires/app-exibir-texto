e8f14d4876e15398d96885228ca1f9b5258268bd
thainaspires <thainaspiress2@gmail.com>
2017-06-09 02:45:01 -0300

    App Exibir Frases
    Aplicativo que exibe frases de pasta do firebase.

---
+++ ExibirFrases/app/src/main/java/com/thaina/exibirfrases/MainActivity.java
@@ -0,0 +1,70 @@
+package com.thaina.exibirfrases;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    String frase1, frase2, frase3;
+    Button btn;
+    TextView fra1, fra2, fra3;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        fra1 = (TextView) findViewById(R.id.textView2);
+        fra2 = (TextView) findViewById(R.id.textView3);
+        fra3 = (TextView) findViewById(R.id.textView4);
+        btn = (Button) findViewById(R.id.button2);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myFirebaseRef = database.getInstance().getReference();
+        myFirebaseRef.child("Salute/EN").addValueEventListener(new ValueEventListener() {
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                frase1 = dataSnapshot.getValue(String.class);
+                fra1.setText(frase1);
+
+            }
+
+            public void onCancelled(DatabaseError error) {
+
+            }
+        });
+        myFirebaseRef.child("Salute/ES").addValueEventListener(new ValueEventListener() {
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                frase2 = dataSnapshot.getValue(String.class);
+                fra2.setText(frase2);
+
+            }
+
+            public void onCancelled(DatabaseError error) {
+
+            }
+        });
+        myFirebaseRef.child("Salute/PT-BR").addValueEventListener(new ValueEventListener() {
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                frase3 = dataSnapshot.getValue(String.class);
+                fra3.setText(frase3);
+
+            }
+
+            public void onCancelled(DatabaseError error) {
+
+            }
+        });
+
+
+    }
+
+}
+
